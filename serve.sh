5574eb7955c19246c74302eb89b38a0a15d172c6
Egor Tensin <Egor.Tensin@gmail.com>
2017-08-09 19:44:19 +0300

    serve.sh: add destination directory parameter

--- serve.sh
+++ serve.sh
@@ -1,3 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-bundle exec jekyll serve --watch --force_polling --host 0.0.0.0 --drafts --config _config.yml,_config_dev.yml
+set -o errexit -o nounset -o pipefail
+
+script_name="$( basename -- "${BASH_SOURCE[0]}" )"
+
+case "$#" in
+    0) ;;
+    1)
+        dest_dir="$1"
+        ;;
+    *)
+        echo "usage: $script_name [DEST_DIR]" >&2
+        exit 1
+esac
+
+bundle exec jekyll serve                    \
+    --config _config.yml,_config_dev.yml    \
+    ${dest_dir+--destination "${dest_dir}"} \
+    --drafts                                \
+    --force_polling                         \
+    --host 0.0.0.0                          \
+    --watch
